44d608108c191c29a265f009ea62715d9a1c3ebb
sev <thiago@guarana.io>
2020-08-04 21:25:17 -0300

    testing new env file

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 const Discord = require('discord.js');
-const DaleBabyBot = require('./bot.js')
+const DaleBabyBot = require('./bot.js');
 
 // Initialize Discord Bot
 const client = new Discord.Client();
 const bot = new DaleBabyBot();
 
 client.on('ready', bot.handleStart);
-client.on('message', bot.handleMessage);
+client.on('message', (message) => {
+    bot.handleMessage(message, client);
+});
 
 client.login(bot.authToken);
