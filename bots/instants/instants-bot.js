6c0732de2acdfd1257c7d7580f70a47f8e3c6120
sev <thiago@guarana.io>
2020-08-21 22:21:59 -0300

    adding url recognition to instant command

--- bots/instants/instants-bot.js
+++ bots/instants/instants-bot.js
@@ -9,6 +9,16 @@ class InstantsBot extends Bot {
     instantSearchQuery(args) {
         return args.join(" ");
     }
+    
+    validURL(str) {
+        var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+          '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+          '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+          '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+          '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+          '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+        return !!pattern.test(str);
+      }
 
     findInstants(message, args) {
         const query = this.instantSearchQuery(args);
@@ -17,7 +27,12 @@ class InstantsBot extends Bot {
         }
         console.log(query);
 
-        var url = "https://www.myinstants.com/search/?name=" + query.split(" ").join("+");
+        if (this.validURL(query)) {
+            var url = query;
+        } else {
+            var url = "https://www.myinstants.com/search/?name=" + query.split(" ").join("+");
+        }
+
 
         console.log(url);
 
--- bots/users/joao-bot.js
+++ bots/users/joao-bot.js
@@ -7,7 +7,8 @@ class JoaoBot extends PersonBot {
             "Foda é quando você sofre um acidente e o médico tem que rasgar a roupa",
             "esse dai conhece varios paus",
             "Gostei desse filme aí que ele coloca o nome dos caras de uma forma diferente",
-            "Tá misturando o antigo com o passado"
+            "Tá misturando o antigo com o passado",
+            "Ó o policial travesso"
         ];
     }
 
